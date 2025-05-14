c9312eec4b95ff77481dda562c1d5abf961065f2
Aleksei <vospennikov.aa@phystech.edu>
2025-05-14 13:40:11 +0300

    fixed 4-3-pwm.py

--- 4-3-pwm.py
+++ 4-3-pwm.py
@@ -1,29 +1,34 @@
 import RPi.GPIO as GPIO
 
+pin = 24
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin, GPIO.OUT)
 
-pwm = GPIO.PWM(pin, frequence)
+pwm = GPIO.PWM(pin, 1000)
 
 pwm.start(0)
 
 try:
     while True:
-        dc = int(input("Enter duty cycle coefficient in percent (or q to quit): "))
-        pwm.ChangeDutyCycle(dc)
-        print("Expected voltage: {:.2f} V\n".format(dc / 100 * 3.3))
+        dc = input("Enter duty cycle coefficient in percent (or q to quit): ")
+        try:
+            dc = int(dc)
+            if (dc > 100 or dc < 0):            
+                print("Entered value is not a percentage. Try again.\n")
+            else:                            
+                pwm.ChangeDutyCycle(dc)
+                print("Expected voltage: {:.2f} V\n".format(dc / 100 * 3.3))
 
-except Exception:
-    if dc == 'q':
-        print("You've entered an exit-character. Bye-bye!\n")
-        break
-    else:
-        print("Entered value is not a number. Try again.\n")
+        except Exception:
+            if dc == 'q':
+                print("You've entered an exit-character. Bye-bye!\n")
+                break
+            else:
+                print("Entered value is not a number. Try again.\n")
 
 finally:
     pwm.stop()
     GPIO.output(pin, 0)
     GPIO.cleanup()
-
-
 
